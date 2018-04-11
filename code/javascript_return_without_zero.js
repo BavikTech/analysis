bf89905716ff65064d4f41667ecdd57029f01e9b
BavikTech <bhavik.popli.14cs@bml.edu.in>
2018-04-11 14:46:25 +0530

    Adding javascript question code

---
+++ code/javascript_return_without_zero.js
@@ -0,0 +1,9 @@
+var data = [32, 33, 16, 40];
+
+function checkAdult(data) {
+    return data!= 18;
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = data.filter(checkwithoutzero);
+}
